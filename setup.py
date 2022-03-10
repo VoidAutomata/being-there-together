f114027ff7f3b5b540e45d491f7de77fa5a4d025
VoidAutomata <82106210+VoidAutomata@users.noreply.github.com>
2022-03-10 16:31:09 -0500

    egg

--- setup.py
+++ setup.py
@@ -1,2 +1,9 @@
-import threading
-import socket
+#import threading
+#import socket
+# setup.py
+from setuptools import setup, find_packages
+setup(
+    name = "being-there-together",
+    version = "0.1",
+    packages = find_packages()
+)
